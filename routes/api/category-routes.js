69fd58c06838692a48a4907b0433b681ece725bd
MOX UFO <mox.ufo@gmail.com>
2023-04-19 18:11:34 -0400

    define both get routes in the catergory routes js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,12 +25,34 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product,
+        include: [
+          {
+            model: Tag,
+            through: ProductTag
+          }
+        ]        
+       }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+    // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+
   // create a new category
 });
 
